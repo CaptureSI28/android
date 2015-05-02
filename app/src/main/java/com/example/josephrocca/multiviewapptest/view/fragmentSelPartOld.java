653c5c3ecd90f2ddd5e91374f864c9805800d963
aperdria <amelieperdriaud@MacBook-Air-de-Amelie.local>
2015-05-02 11:58:02 +0200

    Modification des infos des parties existantes (dates)

--- app/src/main/java/com/example/josephrocca/multiviewapptest/view/fragmentSelPartOld.java
+++ app/src/main/java/com/example/josephrocca/multiviewapptest/view/fragmentSelPartOld.java
@@ -3,6 +3,7 @@ package com.example.josephrocca.multiviewapptest.view;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,13 @@ import com.example.josephrocca.multiviewapptest.model.Game;
 import com.example.josephrocca.multiviewapptest.R;
 import com.example.josephrocca.multiviewapptest.server.ServerRequest;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
 
 /**
  * Created by josephrocca on 27/04/15.
@@ -35,10 +41,28 @@ public class fragmentSelPartOld extends Fragment{
         ListView partieList = (ListView) v.findViewById(R.id.partieList);
 
 
+        SimpleDateFormat formatIn = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat formatOut = new SimpleDateFormat("d MMMM yyyy", Locale.FRENCH);
+
         // Création de la liste de remplissage
         List<String> fill_list = new ArrayList<String>();
         for(Game g : games){
-            fill_list.add(g.getName()+" ("+g.getShortDateDeb()+" "+g.getShortDateFin()+") "+(g.getIsPrivate()?"\nprivate":"\npublic"));
+            String content=g.getName()+"\n";
+            try {
+                Date debut = formatIn.parse(g.getShortDateDeb());
+                content = content.concat("Du " + formatOut.format(debut) + " ");
+                Log.d("debut", formatOut.format(debut));
+                Date fin = formatIn.parse(g.getShortDateFin());
+                content = content.concat("au " + formatOut.format(fin));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            content = content.concat(g.getIsPrivate()?"\nPartie privée":"\nPartie publique");
+            fill_list.add(content);
+        }
+
+        if(fill_list.size()==0) {
+            fill_list.add("Pas de partie en cours.");
         }
 
         // Creation de la liste en elle-même
